b46cb82b1a6abdd46f1d0aeed7ab9dfd648ae0c8
harsh gandhi <harshgandhi043@gmail.com>
2021-11-23 00:25:58 +0530

    created user model

---
+++ models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
