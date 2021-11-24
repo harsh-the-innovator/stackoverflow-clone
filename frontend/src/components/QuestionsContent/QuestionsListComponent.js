f264449428ac3278aa931027131d1c5928fe2367
harsh gandhi <harshgandhi043@gmail.com>
2021-11-24 19:43:45 +0530

    questions data fetched and rendered in UI

--- frontend/src/components/QuestionsContent/QuestionsListComponent.js
+++ frontend/src/components/QuestionsContent/QuestionsListComponent.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { List } from "semantic-ui-react";
+import "./questionscontent.css";
 
-const QuestionsList = () => {
+const QuestionsListComponent = ({ questionData }) => {
+  console.log(questionData);
   return (
     <List
       divided
@@ -9,14 +11,30 @@ const QuestionsList = () => {
       verticalAlign="middle"
       style={{ padding: "0 0.5rem" }}
     >
-      <List.Item>
-        <List.Content as="div">
-          <List.Header as="a">Title</List.Header>
-          <List.Description as="a">Description</List.Description>
-        </List.Content>
-      </List.Item>
+      {questionData.map((question) => {
+        return (
+          <List.Item key={question._id}>
+            <List.Content>
+              <List.Header as="a">{question.title}</List.Header>
+              <List.Description>
+                <div className="question-body-style">{question.body}</div>
+              </List.Description>
+            </List.Content>
+            <div className="list-footer">
+              <div className="question-tags">
+                {question.tags.map((tag) => {
+                  return <div key={tag._id}>{tag.tagname}</div>;
+                })}
+              </div>
+              <div className="question-username">
+                {question.posted_by.username}
+              </div>
+            </div>
+          </List.Item>
+        );
+      })}
     </List>
   );
 };
 
-export default QuestionsList;
+export default QuestionsListComponent;
